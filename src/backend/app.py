3d8869a442b4ed28bd2171dfe2ce08b3fc855211
Jacob Dineen <jacobdineen@macbook-air-2.cidse.dhcp.asu.edu>
2023-09-12 16:19:10 -0700

    test

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,2 +1,12 @@
-# -*- coding: utf-8 -*-
-# flask app (backend for site)
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/api/message', methods=['GET'])
+def get_message():
+    return jsonify({'message': 'Hello from the backend!'}), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3001)
