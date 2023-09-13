a9ee657ad77d692990fed4645aaf4f1c2aa289d2
Jacob Dineen <jacobdineen@MacBook-Air-2.local>
2023-09-13 14:23:21 -0700

    dummy docker compose

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,18 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 
 function App() {
-  const [data, setData] = useState(null);
+  const [message, setMessage] = useState('Loading...');
 
   useEffect(() => {
-    fetch("/api/data")
-      .then((res) => res.json())
-      .then((data) => setData(data));
-  }, []);
+    // Update this URL to point to your backend service
+    fetch('http://localhost:3001/api/message')
+      .then(response => response.json())
+      .then(data => setMessage(data.message))
+      .catch(err => setMessage(`Error: ${err}`));
+  }, []);  // Empty dependency array means this useEffect runs once when the component mounts
 
   return (
-    <div>
-      <h1>React Frontend</h1>
-      {data && <p>{data.message}</p>}
+    <div className="App">
+      <h1>{message}</h1>
     </div>
   );
 }
