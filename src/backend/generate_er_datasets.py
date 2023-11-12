41cbd3dc988abdca9285e91076fb312a283f5166
jake <jdineen@asu.edu>
2023-11-12 13:04:12 -0700

    restruct

--- src/backend/generate_er_datasets.py
+++ src/backend/generate_er_datasets.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """Grab all documents from a directory of JSON files and write them to a CSV file.
 Then Grab only documents that have no CVEs in either the cveids_db or cveids_explicit columns.
+Used for entity recognition task
 """
 
 import ast
@@ -24,10 +25,8 @@ def parse_literal(column):
     [1, 2, 3]
     """
     try:
-        # Safely evaluate the string as a Python literal (list)
         return ast.literal_eval(column)
     except ValueError:
-        # Return the value as-is if it's not a string representation of a list
         return column
 
 
@@ -36,22 +35,17 @@ def get_tagged_and_untagged_documents(directory: str, output_csv: str):
     This function takes a directory containing JSON files and writes them to a CSV file.
     Each JSON file is a single line in the CSV file.
     """
-    # Get a list of all json files in the directory
     json_files = [f for f in os.listdir(directory) if f.endswith(".json")]
 
-    # Write headers just once, assuming all JSON files have the same structure
     first_file = True
 
     for file in tqdm(json_files):
         file_path = os.path.join(directory, file)
 
-        # Process each file line by line to handle memory efficiently
         with open(file_path, "r") as f:
             for line in f:
-                # Each line is a complete JSON object
                 data = pd.read_json(line, lines=True)
 
-                # If first file, write header, else append without header
                 if first_file:
                     data.to_csv(output_csv, mode="w", index=False)
                     first_file = False
@@ -64,27 +58,45 @@ if __name__ == "__main__":
     output_csv = "data/test.csv"
     output_cveless_csv = "data/cveless_docs.csv"
     output_cve_csv = "data/cve_docs.csv"
-    run_full = False
+    regen_data = False
 
-    if run_full:
+    if regen_data:
         get_tagged_and_untagged_documents(directory, output_csv)
         logging.info(f"all docs saved to {output_csv}")
 
-    df = pd.read_csv(output_csv)
-    logging.info(f"full df shape: {df.shape}")
-    # Apply the conversion to the DataFrame
-    df["cveids_db"] = df["cveids_db"].apply(parse_literal)
-    df["cveids_explicit"] = df["cveids_explicit"].apply(parse_literal)
+        df = pd.read_csv(output_csv)
+        logging.info(f"full df shape: {df.shape}")
+        # Apply the conversion to the DataFrame
+        df["cveids_db"] = df["cveids_db"].apply(parse_literal)
+        df["cveids_explicit"] = df["cveids_explicit"].apply(parse_literal)
 
-    # Now filter the DataFrame to get rows where both columns have empty lists
-    filtered_df = df[(df["cveids_db"].map(lambda d: len(d) == 0)) & (df["cveids_explicit"].map(lambda d: len(d) == 0))]
-    logging.info(f"cve-less df shape: {filtered_df.shape}")
-    filtered_df.to_csv(output_cveless_csv, index=False)
-    logging.info(f"cve-less df saved to {output_cveless_csv}")
+        # Now filter the DataFrame to get rows where both columns have empty lists
+        filtered_df = df[
+            (df["cveids_db"].map(lambda d: len(d) == 0)) & (df["cveids_explicit"].map(lambda d: len(d) == 0))
+        ]
+        filtered_df.to_csv(output_cveless_csv, index=False)
+        logging.info(f"cve-less df saved to {output_cveless_csv}")
 
-    filtered_df = filtered_df.set_index(df.index[filtered_df.index])
+        filtered_df = filtered_df.set_index(df.index[filtered_df.index])
 
-    # Use the index to filter out the rows in 'filtered_df' from 'df'
-    antiunion_df = df.drop(filtered_df.index)
-    logging.info(f"cve df shape: {antiunion_df.shape}")
-    logging.info(f"cve df saved to {output_cve_csv}")
+        # Use the index to filter out the rows in 'filtered_df' from 'df'
+        antiunion_df = df.drop(filtered_df.index)
+        antiunion_df.to_csv(output_cve_csv, index=False)
+        logging.info(f"cve df saved to {output_cve_csv}")
+
+    cves = pd.read_csv(output_cve_csv)
+    non_cves = pd.read_csv(output_cveless_csv)
+    logging.info(
+        f"""
+    ------------------
+    CVE Dataset Stats
+    ------------------
+    number of documents: {len(cves)}
+    num unique cves: {len(cves['cveids_db'].unique())}
+    avg docs per cve: {len(cves) / len(cves['cveids_db'].unique())}
+    ------------------
+    Unmapped-CVE Dataset Stats
+    ------------------
+    number of documents: {len(non_cves)}
+    """
+    )
