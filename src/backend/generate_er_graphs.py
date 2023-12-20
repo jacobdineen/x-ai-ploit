807fcb4fd437c0ad31ff6b8e31b25f60c1e02886
jake <jdineen@asu.edu>
2023-12-20 09:59:26 -0700

    vectorized graph generation and swapped to fasttext embeddings

--- src/backend/generate_er_graphs.py
+++ src/backend/generate_er_graphs.py
@@ -8,7 +8,7 @@ To be used for downstream tasks such as training a graph convolutional network (
 import argparse
 import concurrent.futures
 import logging
-import pickle
+import os
 import sys
 
 import fasttext.util
@@ -25,6 +25,35 @@ log_format = "%(asctime)s - %(levelname)s - %(message)s"
 logging.basicConfig(level=logging.INFO, format=log_format)
 
 
+def download_nltk_resources():
+    """
+    Downloads NLTK resources if they are not already available.
+
+    This function checks if the required NLTK resources, such as stopwords and wordnet,
+    are available in the NLTK data directories. If any of the resources are missing,
+    they are downloaded using the nltk.download() function.
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
+    # Define the NLTK resource directories
+    nltk_data_path = nltk.data.path
+
+    # Resources to check and download
+    resources = {"corpora/stopwords": "stopwords", "corpora/wordnet": "wordnet"}
+
+    for resource_path, resource_name in resources.items():
+        # Check if each resource is available in any of the NLTK data directories
+        if not any(os.path.exists(os.path.join(path, resource_path)) for path in nltk_data_path):
+            print(f"Downloading NLTK resource: {resource_name}")
+            nltk.download(resource_name)
+        else:
+            print(f"NLTK resource '{resource_name}' already downloaded.")
+
+
 class CVEGraphGenerator:
     """
     Class to take an input dataframe and generate a graph from it.
@@ -34,24 +63,25 @@ class CVEGraphGenerator:
     Args:
         file_path: The path to the CSV file containing CVE data.
         limit: The number of rows to read from the CSV file.
+        fasttext_model: The path to the fasttext model to use for vectorization.
 
     Attributes:
         file_path: The path to the CSV file containing CVE data.
         limit: The number of rows to read from the CSV file.
         graph: The networkx graph.
         cveid_col: The name of the column containing CVE IDs.
+        fasttext_model: The fasttext model to use for vectorization.
 
     """
 
-    def __init__(self, file_path, limit=None):
+    def __init__(self, file_path, fasttext_model: str = "cc.en.300.bin", limit=None):
         self.file_path = file_path
         self.limit = limit
         self.graph = nx.Graph()
         self.cveid_col = "cveids_explicit"
-        self.vectorizer = None
-        self.ft_model = None
-        nltk.download("stopwords")
-        nltk.download("wordnet")
+        fasttext.util.download_model("en", if_exists="ignore")  # 'en' for English
+        self.ft_model = fasttext.load_model(fasttext_model)
+        download_nltk_resources()
 
     def _is_graph_bipartite(self) -> bool:
         """
@@ -74,49 +104,106 @@ class CVEGraphGenerator:
         Returns:
             pd.Series: The preprocessed text.
         """
+
+        def vectorize_doc(doc):
+            """
+            Vectorize a single document using FastText.
+
+            Args:
+                doc (str): A single document to vectorize.
+
+            Returns:
+                np.array: Vector representation of the document.
+            """
+            words = doc.split()
+            word_vectors = [self.ft_model.get_word_vector(word) for word in words]
+            if len(word_vectors) == 0:
+                return np.zeros(self.ft_model.get_dimension())
+            return np.mean(word_vectors, axis=0)
+
         # Convert to lowercase and remove irrelevant characters
         texts = texts.str.lower().str.replace(r"[^a-zA-Z\s]", "", regex=True)
-
         # Remove stopwords
         stop_words = set(stopwords.words("english"))
         stop_words_pattern = r"\b" + r"\b|\b".join(stop_words) + r"\b"
         texts = texts.str.replace(stop_words_pattern, "", regex=True)
-
         # Lemmatization
         lemmatizer = WordNetLemmatizer()
-
-        def lemmatize_text(text):
-            return " ".join([lemmatizer.lemmatize(word) for word in text.split()])
-
-        return texts.apply(lemmatize_text)
-
-    def process_chunk(self, chunk):
-        try:
-            # Preprocess the chunk here
-            chunk[self.cveid_col] = chunk[self.cveid_col].apply(eval)
-            chunk = chunk.explode(self.cveid_col)
-            chunk["content_text"] = self.preprocess_text_series(chunk["content_text"])
-            return chunk
-        except Exception as e:
-            print(f"Error processing chunk: {e}")
-            return pd.DataFrame()  # Return an empty DataFrame in case of error
-
-    def chunk_generator(self, file_path, chunksize):
-        try:
-            for chunk in pd.read_csv(file_path, chunksize=chunksize, nrows=self.limit):
-                yield chunk
-        except StopIteration:
-            return
+        vectorized_lemmatize = np.vectorize(
+            lambda text: " ".join([lemmatizer.lemmatize(word) for word in text.split()])
+        )
+        lemmatized_texts = pd.Series(vectorized_lemmatize(texts.values))
+        # Vectorization
+        # print(lemmatized_texts.values.shape)
+        vectors = np.array([vectorize_doc(doc) for doc in lemmatized_texts.values])
+        logging.info(f"Vectorized {len(vectors)} documents.")
+        logging.info(f"Vector shape: {vectors.shape}")
+        return vectors
 
     def read_and_preprocess(self):
+        """
+        Reads and preprocesses the data from a CSV file.
+
+        Returns:
+            pd.DataFrame: The preprocessed data as a DataFrame.
+        """
+
+        def process_chunk(chunk):
+            """
+            Process a chunk of data by applying necessary transformations and returning the processed chunk.
+
+            Args:
+                chunk (pandas.DataFrame): The chunk of data to be processed.
+
+            Returns:
+                pandas.DataFrame: The processed chunk of data.
+
+            Raises:
+                Exception: If there is an error during the processing.
+
+            """
+            try:
+                chunk[self.cveid_col] = chunk[self.cveid_col].apply(eval)
+                chunk = chunk.explode(self.cveid_col)
+                # Preprocess and vectorize text
+                vectors = self.preprocess_text_series(chunk["content_text"])
+                # Assign vectors to the DataFrame. Each vector becomes a list or a Series.
+                chunk["embedding"] = vectors.tolist()
+                return chunk
+
+            except Exception as e:
+                print(f"Error processing chunk: {e}")
+                return pd.DataFrame()  # Return an empty DataFrame in case of error
+
+        def chunk_generator(file_path, chunksize):
+            """
+            Generator function that reads a CSV file in chunks and yields each chunk.
+
+            Args:
+                file_path (str): The path to the CSV file.
+                chunksize (int): The number of rows to read per chunk.
+
+            Yields:
+                pandas.DataFrame: A chunk of data read from the CSV file.
+
+            Raises:
+                StopIteration: Raised when there are no more chunks to read.
+
+            """
+            try:
+                for chunk in pd.read_csv(file_path, chunksize=chunksize, nrows=self.limit):
+                    yield chunk
+            except StopIteration:
+                return
+
         chunksize = 10000
         futures = []
         processed_chunks = []
 
         with concurrent.futures.ThreadPoolExecutor() as executor:
-            for ix, chunk in enumerate(self.chunk_generator(self.file_path, chunksize)):
+            for ix, chunk in enumerate(chunk_generator(self.file_path, chunksize)):
                 logging.info(f"Processing chunk {ix}")
-                future = executor.submit(self.process_chunk, chunk)
+                future = executor.submit(process_chunk, chunk)
                 futures.append(future)
 
             for future in concurrent.futures.as_completed(futures):
@@ -127,25 +214,7 @@ class CVEGraphGenerator:
         df = pd.concat(processed_chunks, ignore_index=True) if processed_chunks else pd.DataFrame()
         return df
 
-    def vectorize_text(self, df: pd.DataFrame, fasttext_model: str = "cc.en.300.bin"):
-        logging.info("Vectorizing text data with FastText...")
-
-        fasttext.util.download_model("en", if_exists="ignore")  # 'en' for English
-        ft_model = fasttext.load_model(fasttext_model)
-
-        def vectorize_doc(doc):
-            words = doc.split()
-            word_vectors = [ft_model.get_word_vector(word) for word in words]
-            if len(word_vectors) == 0:
-                return np.zeros(ft_model.get_dimension())
-            return np.mean(word_vectors, axis=0)
-
-        vectors = np.array([vectorize_doc(doc) for doc in df["content_text"]])
-
-        self.ft_model = ft_model
-        return vectors
-
-    def create_graph(self, df: pd.DataFrame, vectors: np.array) -> None:
+    def create_graph(self, df: pd.DataFrame) -> None:
         """
         Create a graph from the dataframe.
 
@@ -164,8 +233,8 @@ class CVEGraphGenerator:
         skipped_cves = 0
         skipped_hash = 0
 
-        for index, row in tqdm(df.iterrows()):
-            cve_id, hash_ = row["cveids_explicit"], row["hash"]
+        for _, row in tqdm(df.iterrows()):
+            cve_id, hash_, embedding = row["cveids_explicit"], row["hash"], row["embedding"]
 
             if pd.isna(cve_id):
                 skipped_cves += 1
@@ -180,8 +249,7 @@ class CVEGraphGenerator:
                 cve_node_count += 1
 
             if hash_ not in self.graph:
-                vector = vectors[index]
-                self.graph.add_node(hash_, vector=vector)
+                self.graph.add_node(hash_, vector=embedding)
                 hash_node_count += 1
 
             self.graph.add_edge(cve_id, hash_)
@@ -242,8 +310,8 @@ class CVEGraphGenerator:
 
         # Separate complex node attributes
         for node, data in self.graph.nodes(data=True):
-            if "vector" in data:
-                node_attributes[node] = data.pop("vector")
+            if "embedding" in data:
+                node_attributes[node] = data.pop("embedding")
 
         # Write the graph structure
         nx.write_gml(self.graph, graph_path)
@@ -253,12 +321,8 @@ class CVEGraphGenerator:
         np.savez(attributes_path, **node_attributes)
 
         # Save the vectorizer
-        logging.info(f"Writing vectorizer to {vectorizer_path}...")
-        if self.vectorizer is not None:
-            with open(vectorizer_path, "wb") as f:
-                pickle.dump(self.vectorizer, f)
-        else:
-            logging.warning("Vectorizer is None and will not be written.")
+        logging.info(f"Writing fasttext model to {vectorizer_path}...")
+        self.ft_model.save_model(vectorizer_path)
 
     def load_graph(self, graph_path: str, attributes_path: str, vectorizer_path: str) -> None:
         """
@@ -276,12 +340,10 @@ class CVEGraphGenerator:
         logging.info(f"Loading node attributes from {attributes_path}...")
         with np.load(attributes_path) as data:
             for node, vector in data.items():
-                self.graph.nodes[node]["vector"] = vector
+                self.graph.nodes[node]["embedding"] = vector
 
-        # Load the vectorizer
         logging.info(f"Loading vectorizer from {vectorizer_path}...")
-        with open(vectorizer_path, "rb") as f:
-            self.vectorizer = pickle.load(f)
+        self.ft_model = fasttext.load_model(vectorizer_path)
 
 
 def main(read_path: str, graph_save_path: str, features_path: str, vectorizer_path: str, limit: int = None) -> None:
@@ -298,13 +360,9 @@ def main(read_path: str, graph_save_path: str, features_path: str, vectorizer_pa
     """
     generator = CVEGraphGenerator(read_path, limit=limit)
     df = generator.read_and_preprocess()
-    vectors = generator.vectorize_text(df)
-    generator.create_graph(df, vectors)
+    generator.create_graph(df)
     generator.write_graph(graph_save_path, features_path, vectorizer_path)
     logging.info(f"Graph, features and vectorizer saved to {graph_save_path}")
-
-    # generator.load_graph(graph_save_path, features_path, vectorizer_path)
-    # logging.info(f"Graph, features, and vectorizer loaded from {graph_save_path}")
 
 
 if __name__ == "__main__":
@@ -313,10 +371,10 @@ if __name__ == "__main__":
         "--read_path", type=str, default="data/cve_docs.csv", help="Path to the CSV file containing CVE data."
     )
     parser.add_argument("--graph_save_path", type=str, default="data/graph.gml", help="Path to the nx graph to")
-    parser.add_argument("--feature_save_path", type=str, default="data/features.npz", help="Path to the nx graph to")
     parser.add_argument(
-        "--vectorizer_save_path", type=str, default="data/vectorizer.pkl", help="Path to the nx graph to"
+        "--feature_save_path", type=str, default="data/features.npz", help="Path to the text embeddings to"
     )
+    parser.add_argument("--vectorizer_save_path", type=str, default="data/ft_model.bin", help="Path to the ft model to")
 
     parser.add_argument("--limit", type=int, default=None, help="Limit the number of rows to read from the CSV file.")
     args = parser.parse_args()
